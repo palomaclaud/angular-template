18ae36e625bbd0dbf91080f0e5b910f6147068f3
Paloma Claudino <claudino.paloma@hotmail.com>
2020-07-18 10:15:02 -0300

    creating a generic toolbar

--- src/app/root/root.module.ts
+++ src/app/root/root.module.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { RootComponent } from './root.component';
+import { ToolbarComponent } from './views/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [RootComponent],
+  declarations: [RootComponent, ToolbarComponent],
   imports: [
     /** Angular Modules */
     CommonModule,
@@ -15,6 +20,10 @@ import { RootComponent } from './root.component';
     /** State Module */
 
     /** Material Modules */
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
 
     /** App Modules */
   ],
