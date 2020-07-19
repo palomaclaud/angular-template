c63630ccea87d29c5672712e27e9b9b8dd65d2e0
Paloma Claudino <claudino.paloma@hotmail.com>
2020-07-19 16:05:30 -0300

    creating a theme and updating packages

--- src/app/root/root.module.ts
+++ src/app/root/root.module.ts
@@ -1,15 +1,19 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { RootComponent } from './root.component';
-import { ToolbarComponent } from './views/toolbar/toolbar.component';
+import { NavigationComponent } from './views/navigation/navigation.component';
 
 @NgModule({
-  declarations: [RootComponent, ToolbarComponent],
+  declarations: [RootComponent, NavigationComponent],
   imports: [
     /** Angular Modules */
     CommonModule,
@@ -24,6 +28,10 @@ import { ToolbarComponent } from './views/toolbar/toolbar.component';
     MatIconModule,
     MatButtonModule,
     MatRippleModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserAnimationsModule,
 
     /** App Modules */
   ],
